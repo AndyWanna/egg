27a623add7e0d288d24b327fd435c1c3055479aa
ANDY WANNA <awanna3@gatech.edu>
2025-01-20 18:03:07 -0500

    merge_option() added

--- src/language.rs
+++ src/language.rs
@@ -768,6 +768,28 @@ pub fn merge_min<T: Ord>(to: &mut T, from: T) -> DidMerge {
         }
     }
 }
+
+/// A utility for implementing [`Analysis::merge`]
+/// when the `Data` type is an [`Option`].
+///
+/// Always take a `Some` over a `None`
+/// and calls the given function to merge two `Some`s.
+pub fn merge_option<T>(
+    to: &mut Option<T>,
+    from: Option<T>,
+    merge_fn: impl FnOnce(&mut T, T) -> DidMerge,
+) -> DidMerge {
+    match (to.as_mut(), from) {
+        (None, None) => DidMerge(false, false),
+        (None, from @ Some(_)) => {
+            *to = from;
+            DidMerge(true, false)
+        }
+        (Some(_), None) => DidMerge(false, true),
+        (Some(a), Some(b)) => merge_fn(a, b),
+    }
+}
+
 /// A simple language used for testing.
 #[derive(Debug, Hash, PartialEq, Eq, Clone, PartialOrd, Ord)]
 #[cfg_attr(feature = "serde-1", derive(serde::Serialize, serde::Deserialize))]
