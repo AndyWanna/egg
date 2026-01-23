040d90a849fc7467db25bd45c16ad7219afbcc59
Andy Wanna <awanna3@gatech.edu>
2026-01-23 17:25:20 -0500

    egg mods for find all children etc..

--- src/egraph.rs
+++ src/egraph.rs
@@ -301,6 +301,203 @@ impl<L: Language, N: Analysis<L>> EGraph<L, N> {
         }
     }
 
+            // Passed Test 1
+    /// Function that gets all child IDs of E-Nodes in an E-Class
+    pub fn get_child_ids(&self, eclass_id: Id) -> Vec<Id> {
+        let eclass = &self[eclass_id];
+        let mut child_ids: Vec<Id> = vec![];
+        for enode in eclass.iter() {
+            let child_class = enode.children();
+            child_ids.extend(child_class);
+        }
+    
+        child_ids
+    }
+
+    // Passed Test 1 
+    /// Function that gets all IDs Below an E-Class, that form a sub E-Graph
+    /// // Order of index is top to bottom -> probably? Not true!!! :((
+    pub fn get_all_child_ids(&self, top_class_id: Id) -> IndexSet<Id> {
+
+        let mut sub_egraph_ids: IndexSet<Id> = IndexSet::new();
+        let mut tracking_egraph_ids: IndexSet<Id> = IndexSet::new(); // We will iterate over this to make sure every node is visited and no cycles. 
+        let canonical_id = self.find(top_class_id);
+
+        sub_egraph_ids.insert(canonical_id);
+        tracking_egraph_ids.insert(canonical_id);
+
+        let mut iter = 0;
+
+        while iter < tracking_egraph_ids.len() {
+            let eclass_id = match tracking_egraph_ids.get_index(iter){
+                Some(x) => *x,
+                None => panic!("This should not reachable -- Trying to adress IndexSet out of bounds!")
+            };
+            let child_ids = self.get_child_ids(eclass_id);
+            for e in child_ids {
+                if e != top_class_id { // if its the top class_id we do not want re add -> will cycle infinitely
+                    // insert node to both index_sets
+                    if sub_egraph_ids.shift_remove(&e) {}; // this one we will constantly move nodes around as we visit them -> might have weird cycle vulnerabilities 
+                    sub_egraph_ids.insert(e);
+                    tracking_egraph_ids.insert(e); 
+                }
+            }
+            iter+=1;
+        }
+
+
+        sub_egraph_ids
+    }
+
+    // I think this can handle cycles?
+    fn get_node_all_child_ids(&self, enode: L ) -> IndexSet<Id> {
+        let mut sub_egraph_ids: IndexSet<Id> = IndexSet::new();
+        let clone_node = enode.clone();
+        let node_children = clone_node.children();
+        let top_class_id = self.lookup(enode).unwrap_or_else(|| panic!("Enode not in egraph!!"));
+        
+        for eclass in node_children {
+            sub_egraph_ids.insert(*eclass);
+        } // insert the base set of children ids
+
+        let mut iter = 0;
+        while iter < sub_egraph_ids.len() {
+            let eclass_id = match sub_egraph_ids.get_index(iter){
+                Some(x) => *x,
+                None => panic!("This should not reachable -- Trying to adress IndexSet out of bounds!")
+            };
+
+            if eclass_id == top_class_id {
+                println!("Cycle Detected!!");
+                continue; // We do not want to re-explore this class, just interested if the cycle exists
+            }
+
+            let child_ids = self.get_child_ids(eclass_id);
+            for e in child_ids {
+                let _in_set = sub_egraph_ids.shift_remove(&e); 
+                sub_egraph_ids.insert(e);
+            }
+            iter+=1;
+        }
+
+        sub_egraph_ids
+    }
+
+
+    /// Check if a node is part of a cycle
+    pub fn is_node_cyclic(&self, enode: &L ) -> bool {
+        let mut sub_egraph_ids: IndexSet<Id> = IndexSet::new();
+        // let clone_node = (*enode).clone();
+        let node_children = enode.children();
+        let top_class_id = self.lookup(enode.clone()).unwrap_or_else(|| panic!("Enode not in egraph!!"));
+        println!("top_id = {top_class_id}");
+        for eclass in node_children {
+            sub_egraph_ids.insert(*eclass);
+        } // insert the base set of children ids
+
+        let mut iter = 0;
+        while iter < sub_egraph_ids.len() {
+            let eclass_id = match sub_egraph_ids.get_index(iter){
+                Some(x) => *x,
+                None => panic!("This should not reachable -- Trying to adress IndexSet out of bounds!")
+            };
+
+            if eclass_id == top_class_id {
+                println!("Cycle Detected!! - Detector Function");
+                return true // dont explore further
+            }
+
+            let child_ids = self.get_child_ids(eclass_id);
+            for e in child_ids {
+                let _in_set = sub_egraph_ids.shift_remove(&e); 
+                sub_egraph_ids.insert(e);
+            }
+            iter+=1;
+        }
+
+        false // if we iterate through all children without seeing top_class no cycles
+    }
+
+
+    /// Function that gets all IDs Below an E-Class, that form a sub E-Graph
+    /// // Order of index is top to bottom -> probably? Not true!!! :((
+    pub fn dfs_get_all_child_ids(&self, top_class_id: Id) -> IndexSet<Id> {
+        let mut sub_egraph_ids: IndexSet<Id> = IndexSet::new();
+        let canonical_id = self.find(top_class_id);
+        sub_egraph_ids.insert(canonical_id);
+
+        // let mut iter = 0;
+
+        // while iter < sub_egraph_ids.len() {
+            // let eclass_id = match sub_egraph_ids.get_index(iter){
+            //     Some(x) => *x,
+            //     None => panic!("This should not reachable -- Trying to adress IndexSet out of bounds!")
+            // };
+            let eclass_id = canonical_id;
+            let child_ids = self.get_child_ids(eclass_id);
+            for child in child_ids {
+                let deep_children_ids = self.dfs_get_all_child_ids(child);
+                for e in deep_children_ids {
+                    // this effecitvely adds the element to the end of the index set if it seen later
+                    let _in_set = sub_egraph_ids.shift_remove(&e); 
+                    sub_egraph_ids.insert(e);
+                }
+            }
+        //     iter+=1;
+        // }
+
+        sub_egraph_ids
+    }
+
+    /// Performs the union between two egraphs.
+    /// 
+    /// ANDY MODIFICATION: Return a map of the old_egraph IDs to the new
+    pub fn egraph_union_with_outmap(&mut self, other: &EGraph<L, N>, other_egraph_top_id: Id) -> HashMap<Id, Id> {
+        let mut egraph_id_mapping:HashMap<Id,Id> = HashMap::default();
+
+        // Each Id we add needs to be unique to the original egraph
+        // As a result need to add Ids from the bottom up
+        let other_ids = other.get_all_child_ids(other_egraph_top_id);
+        // we want to iterate in reverse order
+        for class_id in other_ids.iter().rev(){
+            let eclass = &other[*class_id];
+            let mut eclass_nodes = eclass.nodes.clone();
+            for node in eclass_nodes.iter_mut() {
+                // Mutably modify the node to point to new class references
+                let child_classes = node.children_mut();
+                for other_id in child_classes {
+                    let canonical_child_id = other.find(*other_id); // get the canonicalized id
+                    match egraph_id_mapping.get(&canonical_child_id) {
+                        Some(new_id) => {*other_id = *new_id;}, // if the old_id had been mapped update the class to point at the new_id
+                        None => {} // otherwise do nothing?
+                    }
+                }
+            }
+            
+            // add equivelent representations of the class and union to a single id
+            let new_class_id = self.add(eclass_nodes[0].clone());
+            for eq_node in eclass_nodes{
+                let eq_id = self.add(eq_node);
+                self.union(new_class_id, eq_id);
+            }
+
+            // record the new class mapping
+            egraph_id_mapping.insert(*class_id, new_class_id);
+        }
+
+        // for eclass in other.classes() {
+        //     let original_class_id = eclass.id;
+        //     let class_nodes = eclass.nodes.clone();
+        //     let new_class_id = self.add(class_nodes[0].clone());
+
+        //     let pattern_list = other.id
+        //     egraph_id_mapping.insert(original_class_id, new_class_id);
+        // }
+
+        self.rebuild();
+        return egraph_id_mapping;
+    }
+
     fn intersect_classes(
         &self,
         other: &EGraph<L, N>,
@@ -926,6 +1123,29 @@ impl<L: Language, N: Analysis<L>> EGraph<L, N> {
     pub fn dump(&self) -> impl Debug + '_ {
         EGraphDump(self)
     }
+
+    /// Remove an E-Class from an E-graph (unsafe/untested - Andys Implementation)
+    /// WARNING this should only be used when removing nodes that point to this class aswell!
+    pub fn remove(&mut self, id1: Id){
+        self.classes.remove(&id1);
+    }
+
+    /// Remove an E-Node mapping to an ID from an E-graph (unsafe/untested - Andys Implementation)
+    /// WARNING this should only be used when removing nodes that point to this class aswell!
+    fn remove_expr(&mut self, expr: &L){
+        self.memo.remove(expr);
+    }
+
+    /// Remove all E-Node mappings within an E-Class to an ID from an E-graph (unsafe/untested - Andys Implementation)
+    /// WARNING this should only be used when removing nodes that point to this class aswell!
+    pub fn remove_class_exprs(&mut self, id1: Id){
+        for expr in  self.classes[&id1].nodes.clone()
+        {
+            self.remove_expr(&expr);
+        }
+    }
+    
+
 }
 
 impl<L: Language + Display, N: Analysis<L>> EGraph<L, N> {
